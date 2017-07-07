d43b4bea02ca355868e80a542d9b227ff8d80665
Anıl Doğru <anildogru07@gmail.com>
2017-07-07 09:57:37 +0300

    Create tail.php

---
+++ www/class/tail.php
@@ -0,0 +1,11 @@
+</div>
+</div>
+<?php
+$conn->close();
+?>
+<div class="w3-container w3-bottom">
+	<p class="w3-text-gray w3-right w3-animate-opacity">Anıl Doğru <?php echo $now->format('Y'); ?>&reg;</p>
+</div>
+
+</body>
+</html>
