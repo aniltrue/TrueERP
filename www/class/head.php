8bee15703a3ab18358ec98dd638b56e2e21a6911
Anıl Doğru <anildogru07@gmail.com>
2017-07-07 10:17:46 +0300

    Update head.php

--- www/class/head.php
+++ www/class/head.php
@@ -5,12 +5,22 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 <?php
+// Functions
+	
+function CheckRoles($conn, $UserEmail, $PageName) {
+ $Roles = $conn->("SELECT * FROM UserRoles left join UserRoleTypes WHERE UserEmail = '" . $UserEmail . "' AND (PageName = '" . $PageName ."' OR RoleTypeName = 'ROLE_ALL')");
+ return $Roles->num_rows() > 0;
+}
+?>
+	
+<?php
 $conn = new mysqli("localhost:3306", "root", "", "DegerTarim");	
 if($conn->connect_error){
      die('<div class="w3-panel w3-red w3-margin w3-animate-opacity"><h3>Hata!</h3><br /><p>Bağlantı hatası: ' . $conn->connect_error . '<br />Tekrar deneyiniz.</p></div>');
 }
 $conn->query("SET NAMES 'utf8'");
 $now = new DateTime('now');
+
 function DrawFormItem($row, $value) {
 	$now = new DateTime('now');
 	$conn = new mysqli("localhost:3306", "root", "", "DegerTarim");	
@@ -115,25 +125,26 @@ if(!isset($_SESSION["user"]) || !isset($_SESSION["email"])) {
 	include('tail.php');
 	exit;
 }
-$email = $conn->real_escape_string($_SESSION["email"]);
-$sql = "SELECT * FROM User WHERE UserEmail = '" . $email . "'";
-$rslt = $conn->query($sql);
-if($rslt->num_rows <= 0) {
+$UserEmail = $conn->real_escape_string($_SESSION["email"]);
+$sql = "SELECT * FROM User WHERE UserEmail = '" . $UserEmail . "'";
+$result = $conn->query($sql);
+if($result->num_rows <= 0) {
 	session_destroy();
 	echo '<div class="w3-panel w3-red w3-margin w3-animate-opacity"><h3>Dikkat!</h3><br /><p>Üye girişi yapmak için <a href="index.php" class="w3-hover-gray">BURAYA</a> tıklayınız.</p></div>';
 	include('tail.php');
 	exit;
 }
 	
-$rw = $rslt->fetch_assoc();
-$pswrd = md5($rw["UserPassword"] . "_dg");
-if($pswrd != $_SESSION["user"]) {
+$User = $result->fetch_assoc();
+$Password = md5($rw["UserPassword"] . "_dg");
+if($Password != $_SESSION["user"]) {
 	session_destroy();
 	echo '<div class="w3-panel w3-red w3-margin w3-animate-opacity"><h3>Dikkat!</h3><br /><p>Üye girişi yapmak için <a href="index.php" class="w3-hover-gray">BURAYA</a> tıklayınız.</p></div>';
 	include('tail.php');
 	exit;
 }
 $userInfo = array($rw["UserName"], $rw["UserSurname"], $rw["UserPhone"], $rw["StartDate"]);
+
 ?>
 
 <div class="w3-container w3-border-bottom w3-border-lime w3-teal">
@@ -143,21 +154,20 @@ $userInfo = array($rw["UserName"], $rw["UserSurname"], $rw["UserPhone"], $rw["St
 
 <div class="w3-container w3-green w3-border w3-border-teal w3-margin">
 	<div class="w3-bar w3-border-teal">
-    <a href="main.php" class="w3-bar-item w3-button w3-animate-right">Anasayfa</a>
     <?php
 	
-	$mainmenus = $conn->query("SELECT * FROM MainMenu WHERE MainMenuParent = 0");
+	$mainmenus = $conn->query("SELECT * FROM MainMenu left join (UserRoleTypes natural join Pages) WHERE MainMenuParent = 0");
 	while($mainmenu = $mainmenus->fetch_assoc()) {
-		if(!empty($mainmenu["MainMenuLink"]))
-			echo '<a href="' . $mainmenu["MainMenuLink"] . '" class="w3-bar-item w3-button w3-animate-right">' . $mainmenu["MainMenuName"] . '</a>';	
+		if(!empty($mainmenu["PageURL"]))
+			echo '<a href="' . $mainmenu["PageURL"] . '" class="w3-bar-item w3-button w3-animate-right">' . $mainmenu["MainMenuText"] . '</a>';	
 		else {
 			echo '<div class="w3-dropdown-hover">
-			<button class="w3-button w3-animate-right">' . $mainmenu["MainMenuName"] . '</button>
+			<button class="w3-button w3-animate-right">' . $mainmenu["MainMenuText"] . '</button>
             <div class="w3-dropdown-content w3-bar-block w3-card-4">';
 			
 			$childmenus = $conn->query("SELECT * FROM MainMenu WHERE MainMenuParent = " . $mainmenu["MainMenuID"]);
 			while($childmenu = $childmenus->fetch_assoc()) 
-				echo '<a href="' . $childmenu["MainMenuLink"] . '" class="w3-bar-item w3-button">' . $childmenu["MainMenuName"] . '</a>';
+				echo '<a href="' . $childmenu["PageURL"] . '" class="w3-bar-item w3-button">' . $childmenu["MainMenuText"] . '</a>';
 			
 			echo '</div>
 			</div>';
