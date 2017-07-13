3bcede3f272cad00e3b4579900e3a0761326cc92
Anıl Doğru <anildogru07@gmail.com>
2017-07-13 14:08:25 +0300

    Update AddPage.php

--- www/AddPage.php
+++ www/AddPage.php
@@ -11,8 +11,6 @@ $AddObjects[0] = new AddObject("PageName", "Sayfa Adı", InputTypes::Text, Objec
 $AddObjects[1] = new AddObject("PageURL", "Sayfa Adresi", InputTypes::Text, ObjectTypes::Common);
 $AddObjects[1]->PlaceHolder = "page.php";
 $AddObjects[2] = new AddObject("PageDescription", "Sayfa Açıklaması", InputTypes::Text, ObjectTypes::Common);
-$AddObjects[3] = new AddObject("PageEnable", "Sayfa Onayı", InputTypes::CheckBox, ObjectTypes::Common);
-$AddObjects[3]->PlaceHolder = "Sayfanın kullanılabilmesi için onaylanması lazım.";
 
 ?>
 
