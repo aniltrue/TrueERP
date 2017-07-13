5bd289bd6694aecaafba00a0b5bd5411a54fc476
Anıl Doğru <anildogru07@gmail.com>
2017-07-13 14:10:20 +0300

    Update AddUserTitle.php

--- www/AddUserTitle.php
+++ www/AddUserTitle.php
@@ -2,9 +2,9 @@
 include('class/head.php');
 include('class/AddClass.php');
 $AddObjects = array();
-$TableName = "";
-$ObjectName = "";
-$PageName = "";
+$TableName = "UserTitles";
+$ObjectName = "Ünvan";
+$PageName = "PAGE_HRM_USER_TITLES_ADD";
 // Create "Add Objects"
 
 $AddObjects[0] = new AddObject("TitleName", "Ünvan Adı", InputTypes::Text, ObjectTypes::Common);
