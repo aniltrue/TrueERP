2ff073c8346dfac14bddbe418a4aaabae3614b70
Anıl Doğru <anildogru07@gmail.com>
2017-07-13 14:21:59 +0300

    Create AddMainMenu.php

---
+++ www/AddMainMenu.php
@@ -0,0 +1,15 @@
+
+<?php
+include('class/head.php');
+include('class/AddClass.php');
+$AddObjects = array();
+$TableName = "";
+$ObjectName = "";
+$PageName = "";
+// Create "Add Objects"
+
+
+
+?>
+
+<?php include("class/Add.php"); ?>
