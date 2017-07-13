fe3bc00e1e18acd05c6b865d798ef9a75474af4b
Anıl Doğru <anildogru07@gmail.com>
2017-07-13 11:00:29 +0300

    Update AddUser.php

--- www/AddUser.php
+++ www/AddUser.php
@@ -11,10 +11,9 @@ $AddObjects[1] = new AddObject("UserName", "Kullanıcı Adı", InputTypes::Text,
 $AddObjects[2] = new AddObject("UserSurname", "Kullanıcı Soyadı", InputTypes::Text, ObjectTypes::Common);
 $AddObjects[3] = new AddObject("UserPassword", "Parola", InputTypes::Password, ObjectTypes::Common);
 $AddObjects[4] = new AddObject("UserPhone", "Telefon Numarası", InputTypes::Text, ObjectTypes::Common);
-$AO = new AddObject("TitleName", "Ünvan", InputTypes::ComboBox, ObjectTypes::Common);
-$AO->ComboHelp = new ComboHelp($conn, "SELECT * FROM UserTitles", "TitleName", "TitleDescription");
-$AO->PlaceHolder = "Bir ünvan seçiniz.";
-$AddObjects[5] = $AO;
+$AddObjects[5] = new AddObject("TitleName", "Ünvan", InputTypes::ComboBox, ObjectTypes::Common);
+$AddObjects[5]->ComboHelp = new ComboHelp($conn, "SELECT * FROM UserTitles", "TitleName", "TitleDescription");
+$AddObjects[5]->PlaceHolder = "Bir ünvan seçiniz.";
 
 ?>
 
