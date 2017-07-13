732e891eb3b0632fa80383d96a2e805fda38d31a
Anıl Doğru <anildogru07@gmail.com>
2017-07-13 11:58:11 +0300

    Create AddUserRoleType.php

---
+++ www/AddUserRoleType.php
@@ -0,0 +1,20 @@
+
+<?php
+include('class/head.php');
+include('class/AddClass.php');
+$AddObjects = array();
+$TableName = "";
+$ObjectName = "";
+$PageName = "";
+// Create "Add Objects"
+
+$AddObjects[0] = new AddObject("RoleName", "Rol Adı", InputTypes::Text, ObjectTypes::Common);
+$AddObjects[0]->PlaceHolder = "ROLE_DESCRIPTION_OPERATION";
+$AddObjects[1] = new AddObject("PageName", "Sayfa Adı", InputTypes::ComboBox, ObjectTypes::Common);
+$AddObjects[1]->ComboHelp = new ComboHelp($conn, "SELECT * FROM Pages", "PageName", "PageDescription");
+$AddObjects[1]->PlaceHolder = "Bir sayfa seçiniz.";
+$AddObjects[2] = new AddObject("RoleDescription", "Rol Açıklaması", InputTypes::Text, ObjectTypes::Common);
+
+?>
+
+<?php include("class/Add.php"); ?>
