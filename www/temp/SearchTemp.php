1bed72086c3d88f818b8102471e0db033f77f3ed
Anıl Doğru <anildogru07@gmail.com>
2017-07-14 14:52:03 +0300

    Create SearchTemp.php

---
+++ www/temp/SearchTemp.php
@@ -0,0 +1,17 @@
+<?php
+include('class/head.php');
+include('class/SearchClass.php');
+
+$ObjectName = "";
+$PageName = "";
+$SearchSQL = "";
+$SpecialSQL = "";
+$TableHeaders = array();
+$SearchObjects = array();
+// Create "Table Headers" and "Search Objects"
+
+
+
+?>
+
+<?php include('class/Search.php');
