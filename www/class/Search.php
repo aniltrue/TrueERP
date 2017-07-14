ca2262d68ac39d450d2c486bc7c74fcce8840f3c
Anıl Doğru <anildogru07@gmail.com>
2017-07-14 10:31:04 +0300

    Update Search.php

--- www/class/Search.php
+++ www/class/Search.php
@@ -1,5 +1,6 @@
 <?php
 
+// Search box
 $SearchKey = "";
 if(isset($_GET["SearchKey"])) 
 	$SearchKey = trim($_GET["SearchKey"]);
@@ -16,14 +17,61 @@ echo '<input name="SearchKey" class="w3-input w3-border" type="search" placehold
 
 <?php
 
+// Check Search
 if(!isset($_GET["SearchKey"])) {
 	$conn->close();
 	include('tail.php');
 	exit;
 }
 
-$Rows = search($conn, $SearchSQL, $SpecialSQL);
+// Get Rows
+$Rows = array();
+$Keys = explode(" ", $SearchKey);
 
+$i = 0;
+$rslt1 = $conn->query($SearchSQL);
+while($row = $rslt1->fetch_assoc()) {
+	if(empty($SearchKey)) {
+		$Rows[$i] = $row;
+		$i++;
+		continue;
+	}
+	
+	$Text = "";
+	foreach($row as $r) {
+		$Text = $Text . " " . $r;
+	}
+	
+	if(!empty($SpecialSQL)) {
+		$rslt2 = $conn->query("SELECT * FROM (Product natural join Farmer) natural join (Village natural join District)");
+		
+		if($rslt2 == true) {
+			while($r = $rslt2->fetch_assoc()) {
+				if ($r["TCID"] != $row["TCID"])
+					continue;
+				
+				$Text = $Text . " " . $r["ProductTypeName"] . " " . $r["CityName"] . " " . $r["DistrictName"] . " " . $r["VillageName"];
+			}
+		}
+	}
+	
+	$Text = trim($Text);
+	$IsValid = true;
+
+	foreach($Keys as $Key) {
+		if(!strpos($Text, $Key)) {
+			$IsValid = false;
+			break;	
+		}
+	}
+
+	if($IsValid) {
+		$Rows[$i] = $row;
+		$i++;
+	}
+}
+
+// Write results
 if(count($Rows) == 0) {
 	echo '<p class="w3-center">Aradığınız kriterlerde sonuç bulunamadı.</p>';
 	include('tail.php');
@@ -31,6 +79,7 @@ if(count($Rows) == 0) {
 }
 echo '<p class="w3-center">Toplam ' . count($Rows) . ' sonuç bulundu.</p>';
 
+// Draw tables
 echo '<table class="w3-table-all" id="results">';
 
 echo '<tr class="w3-teal">';
@@ -58,13 +107,13 @@ foreach($Row as $Rows) {
 <script src="//cdn.rawgit.com/rainabba/jquery-table2excel/1.1.0/dist/jquery.table2excel.min.js"></script>
 
 <script type="text/javascript">
-	$("#toExcel").click(function(){
-		$("#results").table2excel({
-    		exclude: ".noExl",
-    		name: <?php echo '"' . $ObjectName . '"'; ?>,
-    		filename: <?php echo '"' . $ObjectName . '"'; ?>
-		});
+$("#toExcel").click(function(){
+	$("#results").table2excel({
+   		exclude: ".noExl",
+   		name: <?php echo '"' . $ObjectName . '"'; ?>,
+   		filename: <?php echo '"' . $ObjectName . '"'; ?>
 	});
+});
 </script>
 
 <?php include('tail.php'); ?>
