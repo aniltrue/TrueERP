d56dac6050966f828f54bda95103d395e6fd0ec6
Anıl Doğru <anildogru07@gmail.com>
2017-07-14 12:06:30 +0300

    Update SearchClass.php

--- www/class/SearchClass.php
+++ www/class/SearchClass.php
@@ -1,7 +1,25 @@
 <?php
 
+class TableHeader {
+ var $Text, $IsExl;
+ 
+ __construct($Text, $IsExl) {
+  $this->Text = $Text;
+  $this->IsExl = $IsExl;
+ }
+}
+
 class SearchObject {
+  var $ColumnName, $IsExl;
   
+  __construct($ColumnName, $IsExl) {
+   $this->ColumnName = $ColumnName;
+   $this->IsExl = $IsExl;
+  }
+  
+  draw($Rows) {
+   return $Rows[$this->ColumnName]; 
+  }
 }
 
 ?>
