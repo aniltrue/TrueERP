c380050419818ab3d30fd8ad8ee3386c2953b971
Anıl Doğru <anildogru07@gmail.com>
2017-07-14 15:33:36 +0300

    Create SearchUser.php

---
+++ www/SearchUser.php
@@ -0,0 +1,16 @@
+<?php
+include('class/head.php');
+include('class/SearchClass.php');
+$ObjectName = "";
+$PageName = "";
+$SearchSQL = "";
+$SpecialSQL = "";
+$TableHeaders = array();
+$SearchObjects = array();
+// Create "Table Headers" and "Search Objects"
+
+
+
+?>
+
+<?php include('class/Search.php');
