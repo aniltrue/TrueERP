6f2ee7f50315ef9d9f7e7d6c0a2334aeb4b285c3
Anıl Doğru <anildogru07@gmail.com>
2017-07-27 14:16:33 +0300

    Update SearchTitleRoles.php

--- www/SearchTitleRoles.php
+++ www/SearchTitleRoles.php
@@ -1,1 +1,14 @@
 
+<?php
+include('class/head.php');
+include('class/SearchClass.php');
+$ObjectName = "";
+$PageName = "";
+$SearchSQL = "";
+$OrderSQL = "";
+$InputObjects = array();
+$SearchObjects = array();
+// Create "Input Objects" and "Search Objects"
+?>
+
+<?php include('class/Search.php'); ?>
