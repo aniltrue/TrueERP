480356c0c98e6eda22a5c7eb0455e66d87034768
Anıl Doğru <anildogru07@gmail.com>
2017-07-27 11:35:46 +0300

    Update logout.php

--- www/logout.php
+++ www/logout.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Değer İyi Tarım - ERP</title>
+<title>TRUE - ERP</title>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 
@@ -10,6 +10,7 @@
 <?php
 	session_start();
 	session_destroy();
+	$now = new DateTime("now");
 ?>
 
 <div class="w3-panel w3-green w3-margin w3-animate-opacity"><h3>Tebrikler!</h3><p>Başarıyla çıkış yaptınız.<br />3 saniye içinde yönlendirileceksiniz.<br /></p></div>
@@ -18,7 +19,7 @@
 ?>
 
 <div class="w3-container w3-display-bottomright">
-	<p class="w3-text-gray w3-opacity w3-right-align w3-animate-opacity">Anıl Doğru 2017&reg;</p>
+	<p class="w3-text-gray w3-opacity w3-right-align w3-animate-opacity">Anıl Doğru <?php echo $now->format('Y'); ?>&reg;</p>
 </div>
 </body>
 </html>
