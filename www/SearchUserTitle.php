108023db3e7ca1223743dc502e4c0b7345626617
Anıl Doğru <anildogru07@gmail.com>
2017-07-27 10:43:01 +0300

    Update SearchUserTitle.php

--- www/SearchUserTitle.php
+++ www/SearchUserTitle.php
@@ -17,6 +17,7 @@ $SearchObjects[0] = new SearchObject("TitleName", "Ünvan Adı", true);
 $SearchObjects[1] = new SearchObject("TitleDescription", "Ünvan Açıklaması", true);
 $SearchObjects[2] = new LinkObject("Rolleri Görüntüle", "PAGE_HRM_TITLE_ROLES_SEARCH", $conn, $userInfo[2], "TitleName");
 $SearchObjects[2]->AdditionalPar = "Search=1";
+$SearchObjects[2]->IsPopup = true;
 $SearchObjects[3] = new LinkObject("Rol Ekle", "PAGE_HRM_TITLE_ROLES_ADD", $conn, $userInfo[2], "TitleName");
 $SearchObjects[4] = new LinkObject("Güncelle", "PAGE_HRM_USER_TITLES_UPDATE", $conn, $userInfo[2], "TitleName");
 $SearchObjects[5] = new LinkObject("Sil", "PAGE_HRM_USER_TITLES_REMOVE", $conn, $userInfo[2], "TitleName");
