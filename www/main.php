0d01d1044a13adbbcae45b79ab17ba0902e6da3d
Anıl Doğru <anildogru07@gmail.com>
2017-07-27 10:15:55 +0300

    Update main.php

--- www/main.php
+++ www/main.php
@@ -1,10 +1,19 @@
-<?php include('class/head.php'); ?>
+<?php include('class/head.php'); 
+
+// Check Roles
+if(!CheckPageRoles($conn, $userInfo[2], $PageName)) {
+	echo '<div class="w3-panel w3-red w3-margin w3-animate-opacity"><h3>Bu Sayfaya Yetkiniz Yok!</h3><br /><p>Anasayfaya dönmek için <a href="main.php" class="w3-hover-gray">BURAYA</a> tıklayınız.</p></div>';
+	include('tail.php');
+	exit;
+}
+
+?>
 <h3 class="w3-text-green">Ana Sayfa</h3>
 
 <p>Sayın <?php echo $userInfo[0] . ' ' . $userInfo[1] ?>,
 <br /><br />
-Değer İyi Tarım kurumsal yazılımına hoşgeldiniz.<br />
-Bu yazılımla üretici, ürün ve analiz bilgileri ekleyebilir, bu bilgileri düzenleyebilir ve silebilirsiniz. "Akıllı Arama" özelliği ile istediğiniz veriye çok hızlı bir şekilde kolayca ulaşabilirsiniz. Ayrıca; yeni üye oluştuma, değiştirme, üyelik bilgilerini güncelleme gibi üyelik işlemlerini ve gelir-gider görüntüleme, fatura bilgilerini ekleme ve düzenleme, üreticilerden alınan paranın kaydını tutma gibi muhasebe işlemleri de yapabilirsiniz. 
+TRUE kurumsal yazılımına hoşgeldiniz.<br />
+...
 <br /><br />
 Bu yazılımı yukarıdaki menüden istediğiniz işlemi seçip kolayca kullanabilirsiniz.
 <br /><br />
