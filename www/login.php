b8a5e275248ffc1b74a743690b21ff5fd86173af
Anıl Doğru <anildogru07@gmail.com>
2017-07-27 11:37:51 +0300

    Update login.php

--- www/login.php
+++ www/login.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>TRUE ERP</title>
+<title>TRUE - ERP</title>
 
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
