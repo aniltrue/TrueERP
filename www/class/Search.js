db72e4867a6e0e952d65c5ce2605cc04f01e2c0e
Anıl Doğru <anildogru07@gmail.com>
2017-07-27 10:35:05 +0300

    Update Search.js

--- www/class/Search.js
+++ www/class/Search.js
@@ -5,9 +5,21 @@ function ToExcel(){
    		filename: <?php echo '"' . $ObjectName . '"'; ?>
 	});
 }
+
 function DisplaySearchPopup() {
 	document.getElementById('SearchPopup').style.display='block';
 }
+
 function CloseSearchPopup() {
 	document.getElementById('SearchPopup').style.display='none';
+}
+
+function PopWindow(url, title) {
+	var width = 1024;
+	var height = 768;
+	var leftPosition, topPosition;
+	leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+	topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+	
+	window.open(url, title, "status=no,height=" + height + ",width=" + width + ",resizable=yes,left=" + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY=" + topPosition + ",toolbar=no,menubar=no,location=no,directories=no");
 }
