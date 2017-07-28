ad43277dcb3a25f1d34d26864bb5f9c76eb2d7b9
Anıl Doğru <anildogru07@gmail.com>
2017-07-28 16:52:34 +0300

    Update SearchRoleType.php

--- www/SearchRoleType.php
+++ www/SearchRoleType.php
@@ -1,13 +1,16 @@
 <?php
 include('class/head.php');
 include('class/SearchClass.php');
-$ObjectName = "";
-$PageName = "";
-$SearchSQL = "";
-$OrderSQL = "";
+$ObjectName = "Kullanıcı Rolü";
+$PageName = "PAGE_DEV_ROLE_TYPES_SEARCH";
+$SearchSQL = "SELECT * FROM UserTitleRoles natural join (UserRoleTypes u left join Pages p ON u.PageName = p.PageName)";
+$OrderSQL = "ORDER BY RoleName, RoleDescription ASC";
 $InputObjects = array();
 $SearchObjects = array();
 // Create "Input Objects" and "Search Objects"
+
+// TODO
+
 ?>
 
 <?php include('class/Search.php'); ?>
