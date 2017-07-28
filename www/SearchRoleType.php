a56f341894c7b75929377142f8e406fb750328e0
Anıl Doğru <anildogru07@gmail.com>
2017-07-28 16:12:39 +0300

    Create SearchRoleType.php

---
+++ www/SearchRoleType.php
@@ -0,0 +1,13 @@
+<?php
+include('class/head.php');
+include('class/SearchClass.php');
+$ObjectName = "";
+$PageName = "";
+$SearchSQL = "";
+$OrderSQL = "";
+$InputObjects = array();
+$SearchObjects = array();
+// Create "Input Objects" and "Search Objects"
+?>
+
+<?php include('class/Search.php'); ?>
