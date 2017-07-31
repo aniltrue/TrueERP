f5a1978b738483b8c1236f88c465cd934ea9398c
Anıl Doğru <anildogru07@gmail.com>
2017-07-31 10:33:35 +0300

    Update Page.php

--- www/Search/Page.php
+++ www/Search/Page.php
@@ -20,6 +20,8 @@ $SearchObjects[1] = new SearchObject("PageDescription", "Sayfa Tanımı", true);
 $SearchObjects[2] = new SearchObject("PageURL", "URL", true);
 $SearchObjects[3] = new SearchObject("PageEnable", "Aktif", true);
 $SearchObjects[4] = new LinkObject("Rolleri Bul", "PAGE_DEV_ROLE_TYPES_SEARCH", $conn, $userInfo[2], "PageName");
+$SearchObjects[4]->AdditionalPar = 'Search=1';
+$SearchObjects[4]->IsPopup = true;
 $SearchObjects[5] = new LinkObject("Güncelle", "PAGE_DEV_PAGE_UPDATE", $conn, $userInfo[2], "PageName");
 
 ?>
