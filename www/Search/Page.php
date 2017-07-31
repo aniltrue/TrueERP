ad2959ac2fce9ee02e06834a53516ff20e8f380c
Anıl Doğru <anildogru07@gmail.com>
2017-07-31 09:43:12 +0300

    Update Page.php

--- www/Search/Page.php
+++ www/Search/Page.php
@@ -1,10 +1,10 @@
 <?php
 include('class/head.php');
 include('class/SearchClass.php');
-$ObjectName = "";
-$PageName = "";
-$SearchSQL = "";
-$OrderSQL = "";
+$ObjectName = "Sayfa";
+$PageName = "PAGE_DEV_PAGE_SEARCH";
+$SearchSQL = "SELECT * FROM Pages";
+$OrderSQL = "ORDER BY PageName, PageDescription ASC";
 $InputObjects = array();
 $SearchObjects = array();
 // Create "Input Objects" and "Search Objects"
