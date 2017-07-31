affde0366dade99362718651126351d9d7edb2b4
Anıl Doğru <anildogru07@gmail.com>
2017-07-31 09:42:30 +0300

    Update Page.php

--- www/Search/Page.php
+++ www/Search/Page.php
@@ -9,7 +9,18 @@ $InputObjects = array();
 $SearchObjects = array();
 // Create "Input Objects" and "Search Objects"
 
+$InputObjects[0] = new InputObject("PageName", "Sayfa Adı", InputTypes::Text);
+$InputObjects[1] = new InputObject("PageDescription", "Sayfa Tanımı", InputTypes::Text);
+$InputObjects[2] = new InputObject("PageURL", "URL", InputTypes::Text);
+$InputObjects[3] = new InputObject("PageEnable", "Aktif", InputTypes::CheckBox);
+$InputObjects[3]->Value = 1;
 
+$SearchObjects[0] = new SearchObject("PageName", "Sayfa Adı", true);
+$SearchObjects[1] = new SearchObject("PageDescription", "Sayfa Tanımı", true);
+$SearchObjects[2] = new SearchObject("PageURL", "URL", true);
+$SearchObjects[3] = new SearchObject("PageEnable", "Aktif", true);
+$SearchObjects[4] = new LinkObject("Rolleri Bul", "PAGE_DEV_ROLE_TYPES_SEARCH", $conn, $userInfo[2], "PageName");
+$SearchObjects[5] = new LinkObject("Güncelle", "PAGE_DEV_PAGE_UPDATE", $conn, $userInfo[2], "PageName");
 
 ?>
 
