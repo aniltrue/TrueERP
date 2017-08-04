4297a5ff8f17811c15997ed3d00d40759febed20
Anıl Doğru <anildogru07@gmail.com>
2017-08-04 10:37:25 +0300

    Create translater.m

---
+++ hw1/translater.m
@@ -0,0 +1,17 @@
+function readableTarget = translater(target, dictionary)
+  target = splitSpaces(target);
+  dictionary = splitSpaces(dictionary);
+  readableText = '';
+  
+  for i = 1:length(target)
+    word = target(i);
+    for j = 1:length(dictionary)
+      if checkWordsSame(word, dictionary(j))
+        word = dictionary(j);
+        break;
+      end
+    end
+    
+    readbleText = [readableText ' ' word];
+  end
+end
