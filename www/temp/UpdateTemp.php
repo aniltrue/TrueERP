c99c4139de4b28bd20ce7f3b083ad841882e7ff7
Anıl Doğru <anildogru07@gmail.com>
2017-08-07 16:39:23 +0300

    Create UpdateTemp.php

---
+++ www/temp/UpdateTemp.php
@@ -0,0 +1,13 @@
+<?php
+$PageName = "";
+include('../class/UpdateClass.php');
+$UpdateObjects = array();
+$TableName = "";
+$ObjectName = "";
+// Create "Update Objects"
+
+
+
+?>
+
+<?php include("../class/Update.php"); ?>
